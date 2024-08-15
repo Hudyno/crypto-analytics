09b50ff6bed2f7319303f0e117340f43536d8fee
Hudyno <hudyyno@gmail.com>
2024-08-15 20:17:22 +0200

    Add dependency to crypto compare api

--- analytics/src/main/java/com/crypto/analytics/AnalyticsApplication.java
+++ analytics/src/main/java/com/crypto/analytics/AnalyticsApplication.java
@@ -1,23 +1,20 @@
 package com.crypto.analytics;
 
-import com.crypto.analytics.service.CoinGeckoApiResponseToJsonSaver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(
 		scanBasePackages = {"com.crypto.**"},
 		nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
 )
+@EnableFeignClients(basePackages = "com.crypto.**")
 @RequiredArgsConstructor
-@EnableAsync
 public class AnalyticsApplication implements ApplicationRunner {
-
-	private final CoinGeckoApiResponseToJsonSaver coinGeckoApiResponseToJsonSaver;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnalyticsApplication.class, args);
@@ -25,6 +22,6 @@ public class AnalyticsApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) {
-//		coinGeckoApiResponseToJsonSaver.saveAllCoinById();
+
 	}
 }
