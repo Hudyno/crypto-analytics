b2d037c963bc08659363e92102912eece763212b
Hudyno <hudyyno@gmail.com>
2024-08-17 15:57:05 +0200

    Change coin mapper to abstract class

--- analytics/src/main/java/com/crypto/analytics/mapper/CoinMapper.java
+++ analytics/src/main/java/com/crypto/analytics/mapper/CoinMapper.java
@@ -7,9 +7,9 @@ import org.mapstruct.Mapper;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface CoinMapper {
+public abstract class CoinMapper {
 
-    Coin fromAssetSummary(AssetSummary assetSummary);
+    public abstract Coin fromAssetSummary(AssetSummary assetSummary);
 
-    List<Coin> fromAssetSummary(List<AssetSummary> assetSummaryList);
+    public abstract List<Coin> fromAssetSummary(List<AssetSummary> assetSummaryList);
 }
