30a30cd06c96da126f1eacff4ff330dea0ab4555
Hudyno <hudyyno@gmail.com>
2024-09-08 18:50:40 +0200

    Add new generic methods

--- persistence/src/main/java/com/crypto/persistence/service/BaseService.java
+++ persistence/src/main/java/com/crypto/persistence/service/BaseService.java
@@ -16,12 +16,30 @@ public abstract class BaseService<T, ID> {
 
     private final JpaRepository<T, ID> repository;
 
+    private static final String ENTITY_NOT_FOUND = "Entity with id: {} not found";
+
     public Optional<T> getReferenceByIdChecked(ID id) {
         if (repository.existsById(id)) {
             return Optional.of(repository.getReferenceById(id));
         }
-        log.info("Entity with id: {} not found", id);
+        logEntityNotFound(id);
         return Optional.empty();
+    }
+
+    public Optional<T> findById(ID id) {
+        Optional<T> entity = repository.findById(id);
+        entity.ifPresentOrElse(
+                it -> log.info("Found entity with id: {}", id),
+                () -> logEntityNotFound(id)
+        );
+
+        return entity;
+    }
+
+    public T save(T entity) {
+        T savedEntity = this.repository.save(entity);
+        log.info("Saved one new entity");
+        return savedEntity;
     }
 
     public void saveAll(List<T> entities) {
@@ -32,6 +50,10 @@ public abstract class BaseService<T, ID> {
         }
         repository.saveAll(cleanedList);
         log.info("Saved {} entities", cleanedList.size());
+    }
+
+    private void logEntityNotFound(ID id) {
+        log.info(ENTITY_NOT_FOUND, id);
     }
 }
 
