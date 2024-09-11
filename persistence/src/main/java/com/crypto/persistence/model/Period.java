71f3b283e7b30f9a6ff8c292ed975b86a5ac81c8
Hudyno <hudyyno@gmail.com>
2024-09-11 09:27:54 +0200

    Add values to Period

--- persistence/src/main/java/com/crypto/persistence/model/Period.java
+++ persistence/src/main/java/com/crypto/persistence/model/Period.java
@@ -1,8 +1,15 @@
 package com.crypto.persistence.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Period {
-    LAST_60D,
-    LAST_120D,
-    LAST_365D,
-    ALL_TIME
+    LAST_60D("60"),
+    LAST_120D("120"),
+    LAST_365D("365"),
+    ALL_TIME(String.valueOf(Integer.MAX_VALUE));
+
+    private final String value;
 }
