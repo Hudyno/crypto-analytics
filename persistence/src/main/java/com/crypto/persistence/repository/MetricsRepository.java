816cb5dfec695a87ed1689f042d42b945e8373b3
Hudyno <hudyyno@gmail.com>
2024-09-11 08:53:21 +0200

    Remove Metrics table

--- persistence/src/main/java/com/crypto/persistence/repository/MetricsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crypto.persistence.repository;
-
-import com.crypto.persistence.model.Metrics;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MetricsRepository extends JpaRepository<Metrics, String> {
-}
